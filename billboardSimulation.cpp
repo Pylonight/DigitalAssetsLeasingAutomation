6d19f73aaefc3a74af6abc114ecd21317645f7fe
Thomas Conlin <Shana@loli.bz>
2018-04-30 18:35:22 +0900

    Bug fix

--- billboardSimulation.cpp
+++ billboardSimulation.cpp
@@ -15,20 +15,21 @@
 #include <climits>
 #include <string>
 #include <cstdio>
+#include <windows.h>
 
 using namespace std;
 
 typedef int address;
 typedef double tokenType;
 typedef time_t timeType;
-typedef int constType;
+typedef double constType;
 
 class Entity;
 class Contract;
 
 unordered_map<address, Entity *> entityList;
 unordered_map<address, Contract *> contractList;
-void transfer(address from, address to, tokenType value);
+bool transfer(address from, address to, tokenType value);
 
 class Entity {
 private:
@@ -76,7 +77,7 @@ public:
 	}
 };
 
-class Billboard : Contract {
+class Billboard : public Contract {
 private:
 	address owner;
 	address ader;
@@ -86,6 +87,7 @@ public:
 	tokenType currentPrice;
 	tokenType lastPrice;
 	timeType expireTime;
+	timeType termInSeconds;
 	bool isIdle;
 	string ad;
 	bool isFirstAder;
@@ -96,11 +98,12 @@ public:
 	constType PRICERISEFACTORU = 11;
 	constType PRICERISEFACTORD = 10;
 
-	Billboard(address sender) {
+	Billboard(address sender, timeType termInSeconds) {
 		owner = sender;
 		rigidPrice = 1;
 		currentPrice = rigidPrice;
 		expireTime = time(0);
+		this->termInSeconds = termInSeconds;
 		isIdle = true;
 		isFirstAder = true;
 		deposit = 0;
@@ -113,6 +116,9 @@ public:
 
 		if (value >= currentPrice) {
 			tokenType previousDeposit = deposit;
+			if (!transfer(sender, addr, value)) {
+				return false;
+			}
 
 			isIdle = false;
 			tokenType toOwner = 0;
@@ -130,7 +136,7 @@ public:
 			lastPrice = value;
 			currentPrice = value*PRICERISEFACTORU/PRICERISEFACTORD;
 
-			expireTime = time(0)+120;
+			expireTime = time(0)+termInSeconds;
 			return true;
 		} else {
 			return false;
@@ -143,6 +149,7 @@ public:
 		}
 		isIdle = true;
 		isFirstAder = true;
+		ad = "";
 		currentPrice /= PRICEDROPFACTOR;
 		if (currentPrice < rigidPrice) {
 			currentPrice = rigidPrice;
@@ -150,7 +157,7 @@ public:
 		tokenType reward = deposit/2;
 		transfer(addr, sender, reward);
 
-		expireTime = time(0)+120;
+		expireTime = time(0)+termInSeconds;
 		return true;
 	}
 
@@ -159,13 +166,21 @@ public:
 	}
 };
 
-void transfer(address from, address to, tokenType value) {
+bool transfer(address from, address to, tokenType value) {
 	if (entityList.find(from) != entityList.end()) {
-		entityList[from]->deposit -= value;
+		if (entityList[from]->deposit >= value) {
+			entityList[from]->deposit -= value;
+		} else {
+			return false;
+		}
 	} else if (contractList.find(from) != contractList.end()) {
-		contractList[from]->deposit -= value;
+		if (contractList[from]->deposit >= value) {
+			contractList[from]->deposit -= value;
+		} else {
+			return false;
+		}
 	} else {
-		return;
+		return false;
 	}
 
 	if (entityList.find(to) != entityList.end()) {
@@ -173,11 +188,48 @@ void transfer(address from, address to, tokenType value) {
 	} else if (contractList.find(to) != contractList.end()) {
 		contractList[to]->deposit += value;
 	} else {
-		return;
+		return false;
 	}
+
+	return true;
+}
+
+void listAllWallets(Entity &owner, vector<Entity *> &candidates, Billboard &billboard) {
+	cout << "Owner: " << owner.deposit << endl;
+	cout << "Alice: " << candidates[0]->deposit << endl;
+	cout << "Boris: " << candidates[1]->deposit << endl;
+	cout << "Cindy: " << candidates[2]->deposit << endl;
+	cout << "David: " << candidates[3]->deposit << endl;
+	cout << "BoardBalance: " << billboard.deposit << endl;
+	cout << "CurrentPrice: " << billboard.currentPrice << endl;
+	cout << "Billboard: " << billboard.ad << endl;
+	cout << endl;
 }
 
 int main() {
-	
+	Entity owner;
+	Billboard billboard(owner.addr, 2);
+	vector<Entity *> candidates(4, NULL);
+	for (int i = 0; i < candidates.size(); ++i) {
+		candidates[i] = new Entity();
+	}
+	candidates[0]->deposit = 3;
+	candidates[1]->deposit = 6;
+	candidates[2]->deposit = 9;
+	candidates[3]->deposit = 0;
+	listAllWallets(owner, candidates, billboard);
+
+	cout << billboard.Offer(candidates[0]->addr, 3, "Alice") << endl;
+	listAllWallets(owner, candidates, billboard);
+
+	cout << billboard.Offer(candidates[1]->addr, 6, "Boris") << endl;
+	listAllWallets(owner, candidates, billboard);
+
+	cout << billboard.Offer(candidates[2]->addr, 9, "Cindy") << endl;
+	listAllWallets(owner, candidates, billboard);
+
+	Sleep(2000);
+	cout << billboard.Expire(candidates[3]->addr) << endl;
+	listAllWallets(owner, candidates, billboard);
 	return 0;
 }
